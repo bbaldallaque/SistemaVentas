c9c5e4660810a9e3332faaa5f49aabc2e1b424ee
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2023-01-21 23:31:08 -0400

    WIP
    Controladores

---
+++ Back-End/APISistemaVentas/SistemaVenta.API/Controllers/RolController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVenta.API.Utilidad;
+using SistemaVenta.BLL.Servicios.Contratos;
+using SistemaVenta.DTO;
+
+namespace SistemaVenta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class RolController : ControllerBase
+    {
+        private readonly IRolService _rolService;
+
+        public RolController(IRolService rolService)
+        {
+            _rolService = rolService;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Lista()
+        {
+            var rsp = new Response<List<RolDTO>>();
+
+            try
+            {
+                rsp.Status = true;
+                rsp.Value = await _rolService.Lista();
+            }
+            catch (Exception ex)
+            {
+
+                rsp.Status = false;
+                rsp.Msg = ex.Message;
+            }
+
+            return Ok(rsp);
+        }
+    }
+}
---
+++ Back-End/APISistemaVentas/SistemaVenta.API/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVenta.API.Utilidad;
+using SistemaVenta.BLL.Servicios;
+using SistemaVenta.BLL.Servicios.Contratos;
+using SistemaVenta.DTO;
+using SistemaVenta.MODEL;
+
+namespace SistemaVenta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet("Lista")]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Lista()
+        {
+            var rsp = new Response<List<UsuarioDTO>>();
+
+            try
+            {
+                rsp.Status = true;
+                rsp.Value = await _usuarioService.Lista();
+            }
+            catch (Exception ex)
+            {
+
+                rsp.Status = false;
+                rsp.Msg = ex.Message;
+            }
+
+            return Ok(rsp);
+        }
+    }
+}
